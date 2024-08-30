f95b1f02e1182ce9fcc67af9c3d5dd39f205a5a7
TranHuuDat <131366672+TranHuuDat2004@users.noreply.github.com>
2024-08-30 18:59:11 +0700

    Add images

--- php/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_unset(); // Xóa tất cả các biến phiên
-session_destroy(); // Hủy phiên hiện tại
-
-// Chuyển hướng người dùng về trang đăng nhập hoặc trang chủ
-header('Location: ../en/product.php');
-exit(); // Đảm bảo không có mã nào khác được thực thi sau khi chuyển hướng
-?>
