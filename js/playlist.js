e8f83c88ada53030e25226230dbd270f6f7e84e3
TranHuuDat <131366672+TranHuuDat2004@users.noreply.github.com>
2025-07-11 18:59:58 +0700

    add reponsive new website

--- js/playlist.js
+++ js/playlist.js
@@ -1,22 +1,47 @@
-// Tạo danh sách phát (Playlist)
-// *** QUAN TRỌNG: Hãy thay đổi các đường dẫn này cho đúng với file của bạn ***
+// *** QUAN TRỌNG: Đã cập nhật theo danh sách nhạc của bạn ***
 const playlist = [
     {
-        title: 'Điều chưa nói',
-        artist: 'Tia ft. OMIXX',
-        coverPath: 'assets/covers/dieu-chua-noi.jpg',
-        audioPath: 'assets/music/dieu-chua-noi.mp3'
+        title: 'On & On',
+        artist: 'Cartoon, Jéja',
+        coverPath: 'assets/covers/on&on.png',
+        audioPath: 'assets/music/Cartoon, Jéja - On & On.mp3'
+    },
+
+    {
+        title: 'All Falls Down',
+        artist: 'Alan Walker',
+        coverPath: 'assets/covers/Alan_Walker_All_Falls_Down.jpg', // Lưu ý tên file ảnh
+        audioPath: 'assets/music/Alan Walker - All Falls Down.mp3'
     },
     {
-        title: 'Tên bài hát 2',
-        artist: 'Nghệ sĩ 2',
-        coverPath: 'assets/covers/song2.jpg',
-        audioPath: 'assets/music/song2.mp3'
+        title: 'Faded',
+        artist: 'Alan Walker',
+        coverPath: 'assets/covers/faded.png',
+        audioPath: 'assets/music/Alan Walker - Faded.mp3'
     },
     {
-        title: 'Tên bài hát 3',
-        artist: 'Nghệ sĩ 3',
-        coverPath: 'assets/covers/song3.jpg',
-        audioPath: 'assets/music/song3.mp3'
+        title: 'The Spectre',
+        artist: 'Alan Walker',
+        coverPath: 'assets/covers/spectre.jpg',
+        audioPath: 'assets/music/Alan Walker - The Spectre (Lyrics).mp3'
+    },
+    {
+        title: 'Lily',
+        artist: 'Alan Walker, K-391 & Emelie Hollow',
+        coverPath: 'assets/covers/lily.jpg',
+        audioPath: 'assets/music/Alan Walker, K-391 & Emelie Hollow - Lily.mp3'
+    },
+
+    {
+        title: 'Way Back Home',
+        artist: 'Conor Maynard',
+        coverPath: 'assets/covers/Way Back Home.jpg',
+        audioPath: 'assets/music/Conor Maynard - Way Back Home.mp3'
+    },
+    {
+        title: 'That Girl',
+        artist: 'Olly Murs',
+        coverPath: 'assets/covers/that girl.png',
+        audioPath: 'assets/music/That Girl - Olly Murs.mp3'
     }
 ];
